da784b7cc721f54c639817bedce113b124195164
Bryan Lund <bryan.helge@gmail.com>
2018-01-08 14:46:37 -0700

    added thumb_image to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108212432) do
+ActiveRecord::Schema.define(version: 20180108214315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180108212432) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "thumb_image"
     t.index ["category_id"], name: "index_restaurants_on_category_id", using: :btree
     t.index ["user_id"], name: "index_restaurants_on_user_id", using: :btree
   end
