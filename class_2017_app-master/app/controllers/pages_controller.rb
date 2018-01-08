5582ef2e590e7929c932f3ee1cc4d9ade44caf85
kemryriggins <98kariggins@gmail.com>
2018-01-08 16:35:48 -0700

    styles

--- class_2017_app-master/app/controllers/pages_controller.rb
+++ class_2017_app-master/app/controllers/pages_controller.rb
@@ -1,11 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @random_rest = get_random_restaurant
   end
 
   def new
   end
 
   def create
+  end
+
+  private
+
+  def get_random_restaurant
+    Restaurant.find([*1..Restaurant.count].sample)
   end
   
 end
