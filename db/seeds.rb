3dbde2c7d3f50618a12ddec79ac003c914dacedc
Bryan Lund <bryan.helge@gmail.com>
2018-01-08 15:47:02 -0700

    updated seed file for categories and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,26 @@
 require 'faker'
 
-10.times do 
+cats = ["Chinese", "Merican", "Mexican", "Mo Bettah", "Chick-Fil-A"]
+
+cats.each do |c|
+  Category.create!(name: c)
+end
+
+3.times do |i|
+  User.create!(
+                email: "asdf#{i}@asdf.com",
+                password: "asdfasdf",
+                password_confirmation: "asdfasdf"
+              )
+end
+
+20.times do 
   Restaurant.create!(name: Faker::RickAndMorty.location,
                      location: Faker::Address.street_address + ',' + Faker::Address.city + ' ' + Faker::Address.state_abbr + ',' + Faker::Address.zip_code,
                       price: Faker::Number.number(1),
                       link: "https://www.website.com",
-                      thumb_image: Faker::LoremPixel.image("150x150")
+                      thumb_image: Faker::LoremPixel.image("150x150"),
+                      category_id: [*1..Category.count].sample,
+                      user_id: [*1..User.count].sample
     )
 end
